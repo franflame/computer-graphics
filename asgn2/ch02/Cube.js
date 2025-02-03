aeb4722cae0bf6f36e54243b46bbe929ed7d0465
franflame <81339239+franflame@users.noreply.github.com>
2025-02-02 22:03:49 -0800

    Update Cube.js

--- asgn2/ch02/Cube.js
+++ asgn2/ch02/Cube.js
@@ -1,13 +1,8 @@
-// reference from: https://github.com/fjtria/cse160/blob/main/asg2/Cube.js
-
 class Cube {
     constructor() {
       this.type='cube';
-    //   this.position = [0.0, 0.0, 0.0];
       this.color = [1.0, 1.0, 1.0, 1.0];
-      //this.size = 5.0;
       this.matrix = new Matrix4();
-      //this.matrix = mat4.create(); // different from tutorial
     }
   
     render() {
@@ -26,67 +21,32 @@ class Cube {
 
       drawTriangle3D( [0.0,0.0,0.0, 0.0,1.0,0.0, 1.0,1.0,0.0]);
 
-      gl.uniform4f(u_FragColor, rgba[0]*.9, rgba[1]*.9, rgba[2]*.9, rgba[3]);      
+      gl.uniform4f(u_FragColor, rgba[0]*.96, rgba[1]*.96, rgba[2]*.96, rgba[3]);      
 
       drawTriangle3D( [0.0,1.0,0.0, 0.0,1.0,1.0, 1.0,1.0,1.0]);
       drawTriangle3D( [0.0,1.0,0.0, 1.0,1.0,1.0, 1.0,1.0,0.0]);
     
-      gl.uniform4f(u_FragColor, rgba[0]*.7, rgba[1]*.7, rgba[2]*.7, rgba[3]);      
+      gl.uniform4f(u_FragColor, rgba[0]*.8, rgba[1]*.8, rgba[2]*.8, rgba[3]);      
 
       drawTriangle3D( [1.0,0.0,0.0, 1.0,1.0,0.0, 1.0,1.0,1.0]);
       drawTriangle3D( [1.0,0.0,1.0, 1.0,1.0,1.0, 1.0,0.0,0.0]);
 
-      gl.uniform4f(u_FragColor, rgba[0]*.65, rgba[1]*.65, rgba[2]*.65, rgba[3]);      
+      gl.uniform4f(u_FragColor, rgba[0]*.94, rgba[1]*.94, rgba[2]*.94, rgba[3]);      
 
       drawTriangle3D( [0.0,0.0,0.0, 1.0,0.0,0.0, 1.0,0.0,1.0]);
       drawTriangle3D( [0.0,0.0,0.0, 0.0,0.0,1.0, 1.0,0.0,1.0]);
     
-      gl.uniform4f(u_FragColor, rgba[0]*.8, rgba[1]*.8, rgba[2]*.8, rgba[3]);      
+      gl.uniform4f(u_FragColor, rgba[0]*.85, rgba[1]*.85, rgba[2]*.85, rgba[3]);      
 
       drawTriangle3D( [0.0,0.0,0.0, 0.0,1.0,0.0, 0.0,1.0,1.0]);
       drawTriangle3D( [0.0,0.0,0.0, 0.0,0.0,1.0, 0.0,1.0,1.0]);
 
-      gl.uniform4f(u_FragColor, rgba[0]*.75, rgba[1]*.75, rgba[2]*.75, rgba[3]);      
+      gl.uniform4f(u_FragColor, rgba[0]*.90, rgba[1]*.90, rgba[2]*.90, rgba[3]);      
 
       drawTriangle3D( [0.0,0.0,1.0, 0.0,1.0,1.0, 1.0,1.0,1.0]);
       drawTriangle3D( [0.0,0.0,1.0, 1.0,0.0,1.0, 1.0,1.0,1.0]);
       gl.uniform4f(u_FragColor, 1.0, 1.0, 1.0, 1.0);
 
-      //   drawTriangle3D( [0.0,0.0,0.0, 0.1,0.1,0.0, 0.1,0.0,0.0]);
-      
-      //   drawTriangle3D( [0.0,0.0,0.0, 0.1,0.1,0.0, 0.1,0.0,0.0]);
-      // drawTriangle3D( [0.0,0.0,0.0, 0.1,0.1,0.0, 0.1,0.0,0.0]);
-      
-      // drawTriangle3D( [0.0,0.0,0.0, 0.0,0.1,0.0, 0.1,0.1,0.0]);
-      
-      
-      // drawTriangle3D( [0.0,0.1,0.0, 0.0,0.1,0.1, 0.1,0.1,0.1]);
-      // drawTriangle3D( [0.0,0.1,0.0, 0.1,0.1,0.1, 0.1,0.1,0.0]);
-      
-      
-      // drawTriangle3D( [0.1,0.0,0.0, 0.1,0.1,0.0, 0.1,0.1,0.1]);
-      // drawTriangle3D( [0.1,0.0,0.1, 0.1,0.1,0.1, 0.1,0.0,0.0]);
-      
-      
-      // drawTriangle3D( [0.0,0.0,0.0, 0.1,0.0,0.0, 0.1,0.0,0.1]);
-      // drawTriangle3D( [0.0,0.0,0.0, 0.0,0.0,0.1, 0.1,0.0,0.1]);
-      
-      
-      // drawTriangle3D( [0.0,0.0,0.0, 0.0,0.1,0.0, 0.0,0.1,0.1]);
-      // drawTriangle3D( [0.0,0.0,0.0, 0.0,0.0,0.1, 0.0,0.1,0.1]);
-      
-      
-      // drawTriangle3D( [0.0,0.0,0.1, 0.0,0.1,0.1, 0.1,0.1,0.1]);
-      // drawTriangle3D( [0.0,0.0,0.1, 0.1,0.0,0.1, 0.1,0.1,0.1]);
-      // Draw
-      //gl.uniform1f(u_Size, size);
-  
-      //gl.drawArrays(gl.POINTS, 0, 1);
-
-      //var d = this.size/200.0;
-
-    //   drawTriangle3D( [0.0,0.0,0.0, 1.0,1.0,0.0, 1.0,0.0,0.0]);
-    //   drawTriangle3D( [0.0,0.0,0.0, 0.0,1.0,0.0, 1.0,1.0,0.0]);
       console.log("drew cube")
 
     }
